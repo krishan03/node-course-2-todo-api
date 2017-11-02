e8728e3bd8c5aba34892ac99ffcb6744f3189e81
Krishan Sharma <krishan.sharma01@nagarro.com>
2017-11-02 20:59:18 +0530

    final version

--- Server/db/mongoose.js
+++ Server/db/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 //instructing moongoose to use promises
 mongoose.Promise=global.Promise;
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
+mongoose.connect(process.env.MONGODB_URI );
 
 module.exports={
     mongoose:mongoose
